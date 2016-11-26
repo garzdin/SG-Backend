ab8b8ef688e541895f2d4c21d88ec34b6c99d88e
Teodor Garzdin <tsg@hugelawn.com>
2016-11-26 18:11:18 +0100

    Add endpoints for CRUD operations for a board. Add a path for saving sensor data.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 var config = require('./config.json');
 var index = require('./controllers/index');
 var authentication = require('./controllers/authentication');
+var boards = require('./controllers/boards');
 
 app.use(bodyParser.json());
 
@@ -24,5 +25,11 @@ app.get('/', index);
 app.post('/register', authentication.register);
 app.post('/login', authentication.login);
 app.use(authentication.middleware);
+app.post('/boards/create', boards.createBoard);
+app.get('/boards', boards.listAllBoards);
+app.get('/boards/:id', boards.listOneBoard);
+app.put('/boards/:id', boards.updateBoard);
+app.delete('/boards/:id', boards.deleteBoard);
+app.post('/boards/:id/reading', boards.saveReading);
 
 app.listen(process.env.PORT || 3000);
---
+++ controllers/boards.js
@@ -0,0 +1,76 @@
+var Board = require('../models/board');
+var Reading = require('../models/reading');
+
+var createBoard = function(request, response) {
+  if(!request.body.name || !request.body.serial) {
+    response.json({"message": "Provide a name and a serial number"});
+  }
+  var board = new Board.model({
+    name: request.body.name,
+    serialNumber: request.body.serial,
+    owner: request.user,
+    dateAdded: new Date()
+  }).save(function(error, board) {
+    if(error) response.json({"message": error});
+    response.json({"message": "Board saved"});
+  });
+}
+
+var listAllBoards = function(request, response) {
+  Board.model.find({ owner: request.user }, function(error, boards) {
+    if(error) response.json({"message": error});
+    response.json({"boards": boards});
+  });
+}
+
+var listOneBoard = function(request, response) {
+  Board.model.findOne({ _id: request.params.id, owner: request.user }, function(error, board) {
+    if(error) response.json({"message": error});
+    response.json({"board": board});
+  });
+}
+
+var updateBoard = function(request, response) {
+  Board.model.findByIdAndUpdate(request.params.id, {
+    name: request.body.name,
+    serialNumber: request.body.serial
+  }, function(error, board) {
+    if(error) response.json({"message": error});
+    response.json({"message": "Board updated successfully"});
+  });
+}
+
+var deleteBoard = function(request, response) {
+  Board.model.findByIdAndRemove(request.params.id, function(error, board) {
+    if(error) response.json({"message": error});
+    response.json({"message": "Board delete successfully"});
+  });
+}
+
+var saveReading = function(request, response) {
+  if(!request.body.humidity || !request.body.temperature || !request.body.light) {
+    response.json({"message": "Provide a humidity, temperature and light indexes"});
+  }
+  Board.model.findOne({ _id: request.params.id, owner: request.user }, function(error, board) {
+    if(error) response.json({"message": error});
+    board.readings.push({
+      humidity: request.body.humidity,
+      temperature: request.body.temperature,
+      light: request.body.light,
+      dateRead: new Date()
+    });
+    board.save(function(error) {
+      if(error) response.json({"message": error});
+      response.json({"message": "Reading saved successfully"});
+    });
+  });
+}
+
+module.exports = {
+  createBoard: createBoard,
+  listAllBoards: listAllBoards,
+  listOneBoard: listOneBoard,
+  updateBoard: updateBoard,
+  deleteBoard: deleteBoard,
+  saveReading: saveReading
+}
