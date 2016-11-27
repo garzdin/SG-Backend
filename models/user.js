571b2e66362aa0d78fd6787262fbc164e36b5490
Teodor Garzdin <tsg@hugelawn.com>
2016-11-27 14:36:10 +0100

    Set the user email to be unique.

--- models/user.js
+++ models/user.js
@@ -13,6 +13,7 @@ var userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     match: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
   },
   password: {
