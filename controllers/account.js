518646f862665c26088f5c7d59a7c8d75a236ff6
Teodor Garzdin <tsg@hugelawn.com>
2016-11-28 12:32:51 +0100

    Properly update the user, only if fields are present.

--- controllers/account.js
+++ controllers/account.js
@@ -1,26 +1,30 @@
-var User = require('../models/user');
-var passwordUtils = require('../utils/crypto');
+var User = require('../models/user')
+var crypto = require('../utils/crypto')
 
 var index = function(request, response) {
   User.model.find({ _id: request.user }, function(error, user) {
-    if (error) response.json({"message": error});
-    return response.json({"user": user});
-  });
+    if (error) response.json({"message": error})
+    return response.json({"user": user})
+  })
 }
 
 var update = function(request, response) {
-  if (!request.body.password) {
-    return response.json({"message": "Please provide a password, atleast"})
-  }
-  User.model.findByIdAndUpdate(request.user, {$set: {
-    firstName: request.body.firstName,
-    lastName: request.body.lastName,
-    password: passwordUtils.encrypt(request.body.password),
-
-  }}, {runValidators: true}, function(error, user) {
-    if (error) return response.json({"message": error});
-    return response.json({"message": "User updated"});
-  });
+  User.model.findOne({_id: request.user}, function(error, user) {
+    if (error) return response.json({"message": error})
+    if (request.body.password) {
+      user.password = crypto.encrypt(request.body.password)
+    }
+    if (request.body.firstName) {
+      user.firstName = request.body.firstName
+    }
+    if (request.body.lastName) {
+      user.lastName = request.body.lastName
+    }
+    user.save(function(error, data) {
+      if (error) return response.json({"message": error})
+      return response.json({"message": "User updated"})
+    })
+  })
 }
 
 module.exports = {
